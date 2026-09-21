0c3a001a6a36c2cec521a65de8261cb089f31c75
MalikaG015 <malikagulati015@gmail.com>
2026-09-21 16:07:09 +0530

    Initial TripSphere project setup

---
+++ src/main/java/com/tripsphere/tripsphere/TripsphereApplication.java
@@ -0,0 +1,13 @@
+package com.tripsphere.tripsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripsphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripsphereApplication.class, args);
+	}
+
+}
